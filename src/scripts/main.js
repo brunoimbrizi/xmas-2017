b0ec5909530238ffe497f3b8b133a0ea6d834c08
Bruno Imbrizi <bruno.imbrizi@gmail.com>
2017-11-20 22:52:49 +0000

    added missing files

---
+++ src/scripts/main.js
@@ -0,0 +1,18 @@
+global.TweenMax = require('gsap');
+global.THREE = require('three');
+require('./../vendors/TrackballControls.js');
+
+import App from './App';
+
+function ready(fn) {
+	if (document.readyState !== 'loading') {
+		fn();
+	} else {
+		document.addEventListener('DOMContentLoaded', fn);
+	}
+}
+
+ready(() => {
+	const app = new App();
+	window.app = app;
+});
